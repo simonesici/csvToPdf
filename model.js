fe712f049611a02bd4de19c70c0c78ba060149d6
simx <simx@MacBook-Pro-di-simx.local>
2024-07-01 11:05:16 +0800

    first commit

---
+++ model.js
@@ -0,0 +1,45 @@
+function CustomerModel(props){
+    var self = this;
+
+    if(props===undefined){
+        props={}
+    }
+
+    self.constructorType = "CustomerModel";
+
+    self.assetId = ko.observable(props[0]);
+    self.assetType = ko.observable(props[1]);
+    self.location = ko.observable(props[2]);
+    self.upsSerialNumber = ko.observable(props[3]);
+    self.equipmentBrand = ko.observable(props[4]);
+    self.equipmentModel = ko.observable(props[5]);
+    self.currentCapacity = ko.observable(props[6]);
+    self.assetNumber = ko.observable(props[7]);
+    self.upsType = ko.observable(props[8]);
+    self.coldStart = ko.observable(props[9]);
+    self.serviceLevel = ko.observable(props[10]);
+    self.result = ko.observable(props[11]);
+
+    self.test = ko.computed(function(){
+        var n = 100;
+        return n+" %";
+    });
+
+    self.getModelData = function() {
+        var data = {
+            assetId: self.assetId(),
+            assetType: self.assetType(),
+            location: self.location(),
+            upsSerialNumber: self.upsSerialNumber(),
+            equipmentBrand: self.equipmentBrand(),
+            equipmentModel: self.equipmentModel(),
+            currentCapacity: self.currentCapacity(),
+            assetNumber: self.assetNumber(),
+            upsType: self.upsType(),
+            coldStart: self.coldStart(),
+            serviceLevel: self.serviceLevel(),
+            result: self.result()
+        };
+        return data;
+    };
+}
---
+++ script.js
@@ -0,0 +1,50 @@
+function FormViewManager() {
+    var self = this;
+    self.id = ko.observable('');
+    self.selectedId = ko.observable('');
+    self.file = ko.observable(null);
+    self.csvData = ko.observableArray([]);
+    self.showInfo = ko.observable(false);
+    self.currentDate = ko.observable('');
+
+    self.fileChanged = function(element, event) {
+        var file = event.target.files[0];
+        self.file(file);
+    };
+
+    self.submitForm = function() {
+        var id = self.id();
+        var file = self.file();
+        var currentDate = new Date().toLocaleString();
+
+        if (file) {
+            var reader = new FileReader();
+            reader.onload = function(e) {
+                var text = e.target.result;
+                var rows = text.split('\n').filter(row => row.trim() !== '');
+                var headers = rows[0].split(',');
+
+                var data = rows.slice(1).map(row => {
+                    var values = row.split(',');
+                    return headers.reduce((obj, header, index) => {
+                        obj[header.trim()] = values[index].trim();
+                        return obj;
+                    }, {});
+                });
+
+                self.csvData(data);
+                self.showInfo(true);
+                self.currentDate(currentDate);
+                self.selectedId(self.id());
+
+                document.getElementById('csvForm').reset();
+                self.id('');
+                self.file(null);
+            };
+            reader.readAsText(file);
+        } else {
+            alert('Per favore, seleziona un file CSV.');
+        }
+    };
+}
+
